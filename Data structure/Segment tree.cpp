3c12432bc46d3dbfcaac728515501ed4195c4546
God-Ra <smitran.godra@gmail.com>
2020-10-30 09:07:16 +0100

    Update segment tree

--- Data structure/Segment tree.cpp
+++ Data structure/Segment tree.cpp
@@ -1,84 +1,125 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
-int a[100010], segTree[400010];
+struct item {
 
-void construct(int low, int high, int pos)
-{
-    if (low == high)
-    {
-        segTree[pos] = a[low];
-        return;
+    item() {}
+    item(int min, int cnt) : m(min), c(cnt) {}
+    int m, c;
+};
+
+struct segtree {
+
+    int size;
+    std::vector<item> values;
+
+    item NEUTRAL_ELEMENT = {1000000001, 0};
+
+    item merge(item a, item b) {
+        if (a.m < b.m) return a;
+        if (a.m > b.m) return b;
+        return {a.m, a.c + b.c};
     }
 
-    int mid = low + (high - low) / 2;
-
-    construct(low, mid, pos * 2 + 1);
-    construct(mid + 1, high, pos * 2 + 2);
-
-    segTree[pos] = std::min(segTree[pos * 2 + 1], segTree[pos * 2 + 2]);
-}
-
-int query(int qlow, int qhigh, int low, int high, int pos)
-{
-    if (low > qhigh || high < qlow)
-        return 1e8;
-
-    if (qlow <= low && high <= qhigh)
-    {
-        return segTree[pos];
+    item build_single(int v) {
+        return {v, 1};
     }
 
-    int mid = low + (high - low) / 2;
-
-    return std::min(query(qlow, qhigh, low, mid, pos * 2 + 1), query(qlow, qhigh, mid + 1, high, pos * 2 + 2));
-}
-
-void update(int node, int value, int low, int high, int pos)
-{
-    if (low > node || high < node)
-        return;
-
-    if (low == high && low == node)
-    {
-        segTree[pos] = value;
-        return;
+    void init(int n) {
+        size = 1;
+        while (size < n) size *= 2;
+        values.resize(2*size);
     }
 
-    int mid = low + (high - low) / 2;
+    void build(std::vector<int> &a, int x, int lx, int rx) {
+        if (lx == rx) {
+            if (lx < (int)a.size()) {
+                values[x] = build_single(a[lx]);
+            }
 
-    update(node, value, low, mid, pos * 2 + 1);
-    update(node, value, mid + 1, high, pos *2 + 2);
+            return;
+        }
 
-    segTree[pos] = std::min(segTree[pos * 2 + 1], segTree[pos * 2 + 2]);
-}
+        int mid = (lx + rx) / 2;
 
-int main()
-{
-    int n, q;
-    std::cin >> n >> q;
+        build(a, 2 * x + 1, lx, mid);
+        build(a, 2 * x + 2, mid + 1, rx);
+        values[x] = merge(values[2*x + 1], values[2*x + 2]);
+    }
 
-    for (int i = 0; i < n; ++i)
-    {
+    void build(std::vector<int> &a) {
+        build(a, 0, 0, size-1);
+    }
+
+    void set(int i, int v, int x, int lx, int rx) {
+        if (lx == rx) {
+            values[x] = build_single(v);
+            return;
+        }
+
+        int mid = (lx + rx) / 2;
+
+        if (i <= mid) {
+            set(i, v, 2*x + 1, lx, mid);
+        } else {
+            set(i, v, 2*x + 2, mid + 1, rx);
+        }
+
+        values[x] = merge(values[2*x + 1], values[2*x + 2]);
+    }
+
+    void set(int i, int v) {
+        set(i, v, 0, 0, size - 1);
+    }
+
+    item min(int l, int r, int x, int lx, int rx) {
+        if (lx > r || rx < l) return NEUTRAL_ELEMENT;
+        if (l <= lx && rx <= r) return values[x];
+
+        int mid = (lx + rx) / 2;
+
+        item left = min(l, r, 2*x + 1, lx, mid);
+        item right = min(l, r, 2*x + 2, mid + 1, rx);
+
+        return merge(left, right);
+    }
+
+    item min(int l, int r){
+        return min(l, r, 0, 0, size-1);
+    }
+};
+
+int main(){
+    std::ios_base::sync_with_stdio(0);
+
+    int n, m;
+    std::cin >> n >> m;
+
+    segtree st;
+    st.init(n);
+    std::vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
         std::cin >> a[i];
     }
 
-    construct(0, n - 1, 0);
+    st.build(a);
+    while(m--) {
+        int op;
+        std::cin >> op;
+        if (op == 1) {
+            int i, v;
+            std::cin >> i >> v;
+            st.set(i, v);
+        } else {
+            int l, r;
+            std::cin >> l >> r;
 
-    while (q--)
-    {
-        int x, one, two;
-        std::cin >> x >> one >> two;
-        --one;
-
-        if (x == 1)
-        {
-            update(one, two, 0, n - 1, 0);
-        }
-        else
-        {
-            --two;
-            std::cout << query(one, two, 0, n - 1, 0) << "\n";
+            --r;
+            item min = st.min(l, r);
+            std::cout << min.m << " " << min.c << "\n";
         }
     }
+
+    return 0;
 }
