c00089d61514910a3963b9277a660fa80ae714c1
pc <smitran.godra@gmail.com>
2023-05-15 22:38:39 +0200

    Modified Sieve of Eratosthenes

--- Math/Sieve of Eratosthenes.cpp
+++ Math/Sieve of Eratosthenes.cpp
@@ -11,16 +11,15 @@ int main()
     {
         is_prime[i] = 1;
     }
+    is_prime[0] = 0;
+    is_prime[1] = 0;
 
     for (int i = 2; i <= n; ++i)
     {
         if (is_prime[i] == 1)
         {
-            if (i * i <= n)
-            {
-                for (int j = i * i; j <= n; j += i)
-                    is_prime[j] = 0;
-            }
+            for (int j = i * i; j <= n; j += i)
+                is_prime[j] = 0;
         }
     }
 }
